bdc9de396063673b2497c1f59bd053de2aaa12eb
Serhii Nikitin <serg.niki@i.ua>
2021-04-29 22:36:17 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9b9b912321e3d315fc8f540d885eee7f.js"
+  "./precache-manifest.4e379e0d3657d5a82e4791630ab6f147.js"
 );
 
 self.addEventListener('message', (event) => {
